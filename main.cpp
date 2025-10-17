06bf68f53737b75847211b22dcb2dac22ad82e32
Aleks <alekslillis2@gmail.com>
2025-10-16 22:57:56 -0400

    attempting to add json library to have better output

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <cpr/cpr.h>
 #include <iostream>
+#include <nlohmann/json.hpp>
 
 using namespace std;
+using json = nlohmann::json;
 
 // cpr knowledge base:
 //https://www.studyplan.dev/pro-cpp/http
@@ -13,6 +15,9 @@ int main() {
                             cpr::Header{{"X-RapidAPI-Key", "461dd69170mshee1346d1aac95c7p13de84jsn5a60f90aed53"},
                                         {"X-RapidAPI-Host", "nfl-api-data.p.rapidapi.com"}});
 
-  cout << r.text;
+  json Doc{json::parse(r.text)};
+
+  cout << Doc.dump(4) << endl;
+  
   return 0;
 }
