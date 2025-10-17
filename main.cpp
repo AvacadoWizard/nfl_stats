9de4965a37445f0c23b6de20cb997f97bd3eba64
AvacadoWizard <128536858+AvacadoWizard@users.noreply.github.com>
2025-10-17 02:42:57 +0000

    tested on github codespace

--- main.cpp
+++ main.cpp
@@ -3,6 +3,11 @@
 
 using namespace std;
 
+// cpr knowledge base:
+//https://www.studyplan.dev/pro-cpp/http
+//nfl api
+//https://rapidapi.com/Creativesdev/api/nfl-api-data
+
 int main() {
   cpr::Response r = cpr::Get(cpr::Url{"https://nfl-api-data.p.rapidapi.com/nfl-livescores"},
                             cpr::Header{{"X-RapidAPI-Key", "461dd69170mshee1346d1aac95c7p13de84jsn5a60f90aed53"},
